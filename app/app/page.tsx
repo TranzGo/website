9e8a259ba6491f30614aeaecf66b7abe7cbbbef6
iammrjude <judeabara@gmail.com>
2024-02-05 19:09:59 +0100

    register

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -13,17 +13,17 @@ function WelcomePage() {
                 />
             </div>
             <div className="flex flex-col items-center justify-center">
-                <h2 className="text-black font-bold text-xl">Welcome to TranzGOO</h2>
+                <h2 className="text-black font-bold text-2xl">Welcome to TranzGOO</h2>
             </div>
             <div className="flex flex-col items-center justify-center text-center mb-10">
                 <p>Your all-in-one telecom solution! Buy airtime, data, pay bills, and convert airtime to cash effortlessly.</p>
                 <p>Your seamless telecom experience begins here. Enjoy!</p>
             </div>
             <div>
-                <button className="text-white font-bold bg-green-600 hover:bg-green-700 py-2 w-64 rounded-2xl mb-5">Create Account</button>
+                <button className="text-white font-bold bg-green-600 hover:bg-green-700 py-2 w-72 rounded-2xl mb-5">Create Account</button>
             </div>
             <div>
-                <button className="text-white font-bold bg-green-600 hover:bg-green-700 py-2 w-64 rounded-2xl mb-5">Login</button>
+                <button className="text-white font-bold bg-green-600 hover:bg-green-700 py-2 w-72 rounded-2xl mb-5">Login</button>
             </div>
         </div>
     );
---
+++ app/app/signin-socials/page.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Image from "next/image";
+import { MdKeyboardArrowLeft as BackIcon } from "react-icons/md"
+import { FcGoogle as GoogleIcon } from "react-icons/fc";
+import { AiOutlineApple as AppleIcon } from "react-icons/ai";
+import { TfiMicrosoftAlt as MicrosoftIcon } from "react-icons/tfi";
+
+function SignInSocials() {
+    return (
+        <div>
+            <div className="flex items-start justify-start text-left mt-8 ml-5">
+                <button><BackIcon className="text-4xl text-green-600 rounded-xl border-solid border-2 border-green-600 hover:bg-green-300" /></button>
+            </div>
+            <div className="flex flex-col items-center justify-center mt-36">
+                <div>
+                    <Image
+                        src="/tranzgoo.png"
+                        alt="TranzGoo Logo"
+                        width={200}
+                        height={48}
+                    />
+                </div>
+                <div>
+                    <button className="flex flex-row items-center justify-center text-center text-green-600 font-bold bg-white hover:bg-green-100 py-2 w-80 rounded-2xl border-solid border-2 border-green-600 mb-7"><GoogleIcon className="text-2xl mr-5" />Continue with Google</button>
+                </div>
+                <div>
+                    <button className="flex flex-row items-center justify-center text-center text-green-600 font-bold bg-white hover:bg-green-100 py-2 w-80 rounded-2xl border-solid border-2 border-green-600 mb-7"><AppleIcon className="text-3xl text-green-600 mr-4" />Continue with Apple</button>
+                </div>
+                <div>
+                    <button className="flex flex-row items-center justify-center text-center text-green-600 font-bold bg-white hover:bg-green-100 py-2 w-80 rounded-2xl border-solid border-2 border-green-600 mb-7"><MicrosoftIcon className="text-2xl mr-5" />Continue with Microsoft</button>
+                </div>
+                <div>
+                    <button className="text-white font-bold bg-green-600 hover:bg-green-700 py-2 w-80 rounded-2xl mb-10">Sign Up</button>
+                </div>
+            </div>
+        </div>
+
+    );
+}
+
+export default SignInSocials;
