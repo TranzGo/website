ddafe8a65668244a72c544dd52724efa682ac953
iammrjude <judeabara@gmail.com>
2024-02-07 10:33:31 +0100

    navbar

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,68 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState } from "react";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+
+function Navbar() {
+    const [showMenu, setShowMenu] = useState(false);
+    function toggleMenu() {
+        setShowMenu(!showMenu);
+    }
+    return (
+        <div className="flex justify-between p-4 max-w-6xl mx-auto">
+            <div className="flex items-center space-x-5">
+                <Link href="/">
+                    <Image
+                        src="/tranzgoo.png"
+                        alt="TranzGoo Logo"
+                        width={150}
+                        height={36}
+                    />
+                </Link>
+                <div className="hidden md:inline-flex items-center space-x-5 text-gray-800 font-bold">
+                    <Link href="/">Home</Link>
+                    <Link href="#">About</Link>
+                    <Link href="#">Blog</Link>
+                    <Link href="#">FAQs</Link>
+                    <Link href="#">Contact Us</Link>
+                </div>
+            </div>
+            <div className="hidden md:inline-flex items-center space-x-2">
+                <Link href="/app/signin-socials" className="text-black font-bold border px-4 py-3 rounded-xl border-black hover:bg-green-100">Sign in</Link>
+                <Link href="/app/register" style={{ "backgroundColor": "#122231" }} className="text-white hover:bg-slate-200 font-semibold rounded-xl px-4 py-3">Create free account</Link>
+            </div>
+            {/* Mobile Button */}
+            <div onClick={toggleMenu} className="flex md:hidden z-10 text-black items-center">
+                {showMenu ? <AiOutlineClose size={28} /> : <AiOutlineMenu size={28} />}
+            </div>
+            {/* Mobile Menu */}
+            <div className={
+                showMenu ?
+                    "md:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-green-700 text-center ease-in duration-300"
+                    :
+                    "md:hidden absolute top-0 left-[-100%] right-0 bottom-0 flex justify-center items-center w-full h-screen bg-green-700 text-center ease-in duration-300"
+            }>
+                <ul>
+                    <li className="p-4 text-4xl hover:text-gray-300">
+                        <Link href="/">Home</Link>
+                    </li>
+                    <li className="p-4 text-4xl hover:text-gray-300">
+                        <Link href="#">About</Link>
+                    </li>
+                    <li className="p-4 text-4xl hover:text-gray-300">
+                        <Link href="#">Blog</Link>
+                    </li>
+                    <li className="p-4 text-4xl hover:text-gray-300">
+                        <Link href="#">Contact Us</Link>
+                    </li>
+                    <li className="p-4 text-4xl hover:text-gray-300">
+                        <Link href="#">FAQs</Link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,11 @@
 import Image from "next/image";
 import Link from "next/link";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center h-screen p-24">
-      <div className="z-10 max-w-5xl w-full items-center font-mono text-sm lg:flex">
-        <p className="text-4xl text-green-600">TranzGOO HomePage</p>
-      </div>
-      <div className="z-10 max-w-5xl w-full items-center font-mono text-sm lg:flex">
-        <Link href="/app">
-          <button className="font-bold bg-green-300 hover:bg-green-600 py-2 w-40 rounded-2xl mt-5">
-            Launch App
-          </button>
-        </Link>
-      </div>
-    </main>
+    <div>
+      <Navbar />
+    </div>
   );
 }
